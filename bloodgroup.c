26acd6fcf567cbdc67bfd6131a1a8ba69216e472
XirisXER <twice2541@gmail.com>
2019-11-21 13:01:38 +0700

    Group?, what is that?

--- bloodgroup.c
+++ bloodgroup.c
@@ -5,11 +5,11 @@ void main()
   int i;
   do
   {
-    printf("YOLOOOOOOOOOOOOOOOOOOO");
+    printf("YOLO");
     scanf("%s", Name);
     do
     {
-      printf("What is your group? ");
+      printf("What is your group?");
       scanf("%s", G);
 
       if (strcmp(G, "A") !=0  &&
